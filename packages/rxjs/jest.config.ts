ac8922b1528477b15703067e107388feb30fb029
Shy Agam <us@bespunky.io>
2022-12-02 18:31:55 +0200

    chore: [nx migration] update-to-export-default

--- packages/rxjs/jest.config.ts
+++ packages/rxjs/jest.config.ts
@@ -1,5 +1,5 @@
 /* eslint-disable */
-module.exports = {
+export default {
   displayName: "rxjs",
   preset: "../../jest.preset.js",
   globals: {
--- packages/typescript-utils/jest.config.ts
+++ packages/typescript-utils/jest.config.ts
@@ -1,5 +1,5 @@
 /* eslint-disable */
-module.exports = {
+export default {
   displayName: "typescript-utils",
   preset: "../../jest.preset.js",
   globals: {
