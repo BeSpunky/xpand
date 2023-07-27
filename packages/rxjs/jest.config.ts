d3546fc058fbfd60e51af52b9136c8d21040f252
Shy Agam <shyagam@gmail.com>
2023-07-27 12:06:44 +0300

    chore: [nx migration] update-configs-jest-29

--- packages/rxjs/jest.config.ts
+++ packages/rxjs/jest.config.ts
@@ -2,16 +2,17 @@
 export default {
   displayName: "rxjs",
   preset: "../../jest.preset.js",
-  globals: {
-    "ts-jest": {
-      tsconfig: "<rootDir>/tsconfig.spec.json",
-    },
-  },
+  globals: {},
   setupFilesAfterEnv: [
     "../../node_modules/@hirez_io/observer-spy/dist/setup-auto-unsubscribe.js",
   ],
   transform: {
-    "^.+\\.[tj]s$": "ts-jest",
+    "^.+\\.[tj]s$": [
+      "ts-jest",
+      {
+        tsconfig: "<rootDir>/tsconfig.spec.json",
+      },
+    ],
   },
   moduleFileExtensions: ["ts", "js", "html"],
   coverageDirectory: "../../coverage/packages/rxjs",
